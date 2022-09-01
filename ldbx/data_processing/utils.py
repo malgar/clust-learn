fc5f090e729a07143964b7175d1e4bb2080426f9
malgar <mglalvarezg@gmail.com>
2022-09-01 08:48:10 +0200

    Data preprocessing docmentation and visualizations

--- ldbx/data_processing/utils.py
+++ ldbx/data_processing/utils.py
@@ -6,19 +6,57 @@ import pandas as pd
 
 from scipy.stats import rv_discrete
 from sklearn.feature_selection import mutual_info_regression, mutual_info_classif
+from sklearn.impute import KNNImputer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mutual_info_score
+from sklearn.preprocessing import MinMaxScaler
 from ..utils import cross_corr_ratio
 
+# TODO: Deletion of variables with a pct of missing values above a given threshold
 
-def compute_missing(df):
-    missing_df = (df.isnull().sum()*100/df.shape[0]).to_frame('pct_missing').reset_index()\
-        .rename(columns={'index': 'var_name'})
-    missing_df = missing_df.sort_values('pct_missing', ascending=False)
+
+def compute_missing(df, normalize=True):
+    """
+    Calculates the pct/count of missing values per column.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+    normalize : boolean, default=True
+
+    Returns
+    ----------
+    missing_df : `pandas.DataFrame`
+        DataFrame with the pct/counts of missing values per column.
+    """
+    missing_df = (df.isnull().sum()).to_frame('missing').reset_index().rename(columns={'index': 'var_name'})
+    if normalize:
+        missing_df['missing'] = missing_df['missing'] * 100 / df.shape[0]
+    missing_df = missing_df.sort_values('missing', ascending=False)
     return missing_df
 
 
 def num_imputation_pairs(df, corr_thres=0.7, method='pearson'):
+    """
+    Computes the correlation coefficient between every pair of variables and returns those pairs with an absolute value
+    above the given threshold.
+    *Note* all variables must be numerical.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame containing the data.
+    corr_thres : float, default=0.7
+        Correlation theshold to consider two variables as strongly correlated.
+    method : str, default='pearson'
+        Method of correlation (pearson, kendall, spearman, or callable function -
+        see https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.corr.html).
+
+    Returns
+    ----------
+    num_pairs : `pandas.DataFrame`
+        DataFrame with pairs of highly correlated variables together with the correlation coefficient value.
+    """
     corr_df = df.corr(method=method)
     # We only keep the pairs with a corr coefficient above the threshold and melt the DataFrame to have a row per pair
     num_pairs = corr_df.replace({1: 0})[np.abs(corr_df.replace({1: 0})) > corr_thres].reset_index()\
@@ -27,7 +65,29 @@ def num_imputation_pairs(df, corr_thres=0.7, method='pearson'):
     return num_pairs
 
 
-def mixed_imputation_pairs(df, num_vars, cat_vars, cross_corr_thres=0.5):
+# TODO: Rename cross correlation with partial eta squared
+def mixed_imputation_pairs(df, num_vars, cat_vars, cross_corr_thres=0.14):
+    """
+    Computes the dependency between pairs of numerical and categorical variables through partial eta squared, and
+    returns those pairs with a value above the given threshold.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame containing the data.
+    num_vars : string, list, series, or vector array
+        Numerical variable name(s).
+    cat_vars : string, list, series, or vector array
+        Categorical variable name(s).
+    cross_corr_thres : float, default=0.14
+        Threshold to consider two variables as strongly related (see
+        https://www.spss-tutorials.com/effect-size/#anova-partial-eta-squared).
+
+    Returns
+    ----------
+    pairs : `pandas.DataFrame`
+        DataFrame with pairs of highly correlated variables together with the partial eta squared value.
+    """
     cross_corr_df = cross_corr_ratio(df[cat_vars], df[num_vars])
     # We keep only the pairs with a cross correlation coefficient above the threshold and melt the notebook to have a
     # row per pair
@@ -43,6 +103,22 @@ def mixed_imputation_pairs(df, num_vars, cat_vars, cross_corr_thres=0.5):
 
 
 def cat_imputation_pairs(df, mi_thres=0.6):
+    """
+    Computes the dependency between paris of categorical variables through mutual information
+    (https://en.wikipedia.org/wiki/Mutual_information), and returns those pairs with a value above the given threshold.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame containing the data.
+    mi_thres : float, default=0.6
+        Threshold to consider two variables as strongly related.
+
+    Returns
+    ----------
+    cat_pairs : `pandas.DataFrame`
+        DataFrame with pairs of highly correlated variables together with the mutual information score.
+    """
     data = []
     cat_vars = list(df.columns)
     for i in range(len(cat_vars) - 1):
@@ -58,44 +134,128 @@ def cat_imputation_pairs(df, mi_thres=0.6):
     return cat_pairs
 
 
-def imputation_pairs(df, num_vars, cat_vars, num_kws=None, mixed_kws=None, cat_kws=None):
+def imputation_pairs(df, num_vars=None, cat_vars=None, num_kws=None, mixed_kws=None, cat_kws=None):
+    """
+    Computes one-to-one model-based imputation pairs. These are strongly related pairs of variables. Depending on the
+    type of variables, a correlation coefficient (numerical variables), partial eta squared (mixed-type variables), or
+    mutual information (categorical variables) is used to measured the strength of the relationship of each pair.
+
+    When one variable is strongly related to more than one variable, the one that allows a larger amount of imputations.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame containing the data.
+    num_vars : str, list, series, or vector array
+        Numerical variable name(s).
+    cat_vars : str, list, series, or vector array
+        Categorical variable name(s).
+    {num,mixed,cat}_kws : dict, default=None
+        Additional keyword arguments to pass to `num_imputation_pairs()`, `mixed_imputation_pairs()`, and
+        `cat_imputation_pairs()`.
+
+    Returns
+    ----------
+    final_pairs : `pandas.DataFrame`
+        DataFrame with pairs of highly correlated variables (var1: variable with values to impute; var2: variable to be
+        used as independent variable for model-based imputation), together proportion of missing values of variables
+        var1 and var2.
+    """
+    if num_vars is None and cat_vars is None:
+        raise ValueError('Numerical and categorical variable lists are required.')
+
     # Numerical variable pairs (correlation)
-    num_kws = num_kws if num_kws else dict()
-    num_pairs = num_imputation_pairs(df[num_vars], **num_kws)
+    num_pairs = pd.DataFrame()
+    if num_vars:
+        num_kws = num_kws if num_kws else dict()
+        num_pairs = num_imputation_pairs(df[num_vars], **num_kws)
 
     # Mixed variable pairs (cross correlation)
-    mixed_kws = mixed_kws if mixed_kws else dict()
-    mixed_pairs = mixed_imputation_pairs(df, num_vars, cat_vars, **mixed_kws)
+    mixed_pairs = pd.DataFrame()
+    if num_vars and cat_vars:
+        mixed_kws = mixed_kws if mixed_kws else dict()
+        mixed_pairs = mixed_imputation_pairs(df, num_vars, cat_vars, **mixed_kws)
 
     # Categorical variable pairs (mutual information)
-    cat_kws = cat_kws if cat_kws else dict()
-    cat_pairs = cat_imputation_pairs(df[cat_vars], **cat_kws)
+    cat_pairs = pd.DataFrame()
+    if cat_vars:
+        cat_kws = cat_kws if cat_kws else dict()
+        cat_pairs = cat_imputation_pairs(df[cat_vars], **cat_kws)
 
-    final_pairs = pd.concat([num_pairs, mixed_pairs, cat_pairs])
+    final_pairs = pd.concat([num_pairs, mixed_pairs, cat_pairs], ignore_index=True)
     final_pairs = final_pairs.drop(columns='value')
+
+    num_vars = num_vars if num_vars else []
+    cat_vars = cat_vars if cat_vars else []
 
     # We only want to keep those pairs with missing values
     missing_df = compute_missing(df[num_vars + cat_vars])
     final_pairs = final_pairs.merge(missing_df, left_on='var1', right_on='var_name').rename(
-        columns={'pct_missing': 'pct_missing_var1'}).drop(columns='var_name')
+        columns={'missing': 'missing_var1'}).drop(columns='var_name')
     final_pairs = final_pairs.merge(missing_df, left_on='var2', right_on='var_name').rename(
-        columns={'pct_missing': 'pct_missing_var2'}).drop(columns='var_name')
-    # Each variable with missing value and strongly related to at least another one, is assigned to the one from which
+        columns={'missing': 'missing_var2'}).drop(columns='var_name')
+    # Each variable with missing values and strongly related to at least another one, is assigned to the one from which
     # a larger proportion of missing values can be imputed.
-    final_pairs = final_pairs.loc[final_pairs.groupby('var1')['pct_missing_var2'].idxmin()].sort_values(
-        ['pct_missing_var2', 'pct_missing_var1'])
-    final_pairs = final_pairs[final_pairs['pct_missing_var1'] > final_pairs['pct_missing_var2']].reset_index(drop=True)
+    final_pairs = final_pairs.loc[final_pairs.groupby('var1')['missing_var2'].idxmin()].sort_values(
+        ['missing_var2', 'missing_var1'])
+    final_pairs = final_pairs[final_pairs['missing_var1'] > final_pairs['missing_var2']].reset_index(drop=True)
     return final_pairs
 
+def _bucketize(values, q=10):
+    """
+    Quantile bucketizer.
 
-def bucket(values, q=10):
+    Parameters
+    ----------
+    values : `numpy.array`, list
+        Array of values to bucketize.
+    q : int, default=10
+        Number of buckets.
+
+    Returns
+    ----------
+    b_values : `pandas.Series`
+        Series with bucketized values.
+    """
     b_values = values
     if len(np.unique(values)) > 10:
         b_values = pd.qcut(values, q=q, duplicates='drop')
     return b_values
 
 
-def impute_missing_values_with_highly_related_pairs(df, num_vars, cat_vars, imputation_pairs):
+def impute_missing_values_with_highly_related_pairs(df, imputation_pairs, num_vars=None, cat_vars=None):
+    """
+    One-to-one model based imputation for the imputation pairs identified.
+    * If both variables are numerical, linear regression is used. The regressor is trained using all observations with
+    known values for both variables.
+    * If both variables are categorical, we use hot deck imputation considering for every recipient all observations
+    with the same value of the independent variable as donors. The value to be imputed is selected at random following
+    the discrete empirical distribution.
+    * For mixed-type variables, the numerical one is discretized by bucketization into quantiles and both variables are
+    treated as categorical.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame containing the data.
+    imputation_pairs : `pandas.DataFrame`
+        Imputation pairs as returned by the function `imputation_pairs()`.
+    num_vars : str, list, series, or vector array
+        Numerical variable name(s).
+    cat_vars : str, list, series, or vector array
+        Categorical variable name(s).
+
+    Returns
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame with imputed data.
+    """
+    if num_vars is None and cat_vars is None:
+        raise ValueError('Numerical and categorical variable lists are required.')
+
+    num_vars = num_vars if num_vars else []
+    cat_vars = cat_vars if cat_vars else []
+
     for idx, row in imputation_pairs.iterrows():
         if row['var1'] in num_vars and row['var2'] in num_vars:
             # If both variables are numerical, use linear regression
@@ -110,14 +270,14 @@ def impute_missing_values_with_highly_related_pairs(df, num_vars, cat_vars, impu
 
         else:
             print(f"Imputing with empirical discrete distribution {row['var2']} -> {row['var1']}")
-            # Bucket the numerical variable to treat both as categorical.
+            # Bucketize the numerical variable to treat both as categorical.
             if row['var2'] in num_vars:
-                # print(f"Bucket variable {row['var2']}")
-                df[f"b_{row['var2']}"] = bucket(df[row['var2']])
+                # print(f"Bucketize variable {row['var2']}")
+                df[f"b_{row['var2']}"] = _bucketize(df[row['var2']])
                 df[f"b_{row['var1']}"] = df[row['var1']]
             elif row['var1'] in num_vars:
-                # print(f"Bucket variable {row['var1']}")
-                df[f"b_{row['var1']}"] = bucket(df[row['var1']])
+                # print(f"Bucketize variable {row['var1']}")
+                df[f"b_{row['var1']}"] = _bucketize(df[row['var1']])
                 df[f"b_{row['var2']}"] = df[row['var2']]
             else:
                 df[f"b_{row['var1']}"] = df[row['var1']]
@@ -129,9 +289,8 @@ def impute_missing_values_with_highly_related_pairs(df, num_vars, cat_vars, impu
             for pv in predictor_values:
                 # For every value of the independent variable, we compute the discrete distribution frequencies of the
                 # dependent variable calculated when both dependent and independent values are non-missing.
-                dist_freq = df.loc[
-                    (df[f"b_{row['var2']}"] == pv) & (~df[row['var1']].isnull()), row['var1']].value_counts(
-                    normalize=True).sort_index()
+                dist_freq = df.loc[(df[f"b_{row['var2']}"] == pv) & (~df[row['var1']].isnull()), row['var1']]\
+                    .value_counts(normalize=True).sort_index()
                 if dist_freq.shape[0] > 0:
                     xk = tuple(dist_freq.index)
                     pk = tuple(dist_freq)
@@ -147,7 +306,30 @@ def impute_missing_values_with_highly_related_pairs(df, num_vars, cat_vars, impu
     return df
 
 
-def mutual_information_pair_scores(df, num_vars, cat_vars):
+def mutual_information_pair_scores(df, num_vars=None, cat_vars=None):
+    """
+    Computes the mutual information score for every pair of variables.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame containing the data.
+    num_vars : str, list, series, or vector array
+        Numerical variable name(s).
+    cat_vars : str, list, series, or vector array
+        Categorical variable name(s).
+
+    Returns
+    ----------
+    mi_scores_df : `pandas.DataFrame`
+        DataFrame with mutual information score for every pair of variables.
+    """
+    if num_vars is None and cat_vars is None:
+        raise ValueError('Numerical and categorical variable lists are required.')
+
+    num_vars = num_vars if num_vars else []
+    cat_vars = cat_vars if cat_vars else []
+
     # First, we calculate max mutual information per variable for normalization
     max_mi_scores = dict()
     for nv in num_vars:
@@ -199,8 +381,27 @@ def mutual_information_pair_scores(df, num_vars, cat_vars):
     return mi_scores_df
 
 
-# TODO: It shouldn't be compulsory to have both types of variables. (This applies to all the modules)
 def variable_graph_partitioning(pair_scores, thres=0.5):
+    """
+    Computes a graph partitioning of the graph G=(V, E), where V = {variables} and E = {pairs of variables with a mutual
+    information score above the threshold}. The connected components represent clusters of related variables.
+
+    Parameters
+    ----------
+    pair_scores : `pandas.DataFrame`
+        DataFrame with mutual information score for every pair of variables.
+    thres : float, default=0.5
+        Threshold to determine if two variables are similar based on mutual information score.
+
+    Returns
+    ----------
+    all_nodes : list
+        DataFrame with mutual information score for every pair of variables.
+    edges : list of tuples
+        List of graph edges
+    g_list : list of sets
+        List of connected components.
+    """
     edges = pair_scores[pair_scores['score'] > thres].apply(lambda row: (row['var1'], row['var2']), axis=1).tolist()
 
     connected_nodes = list(zip(*edges))
@@ -214,3 +415,78 @@ def variable_graph_partitioning(pair_scores, thres=0.5):
     # Nodes, edges, connected components
     all_nodes = list(np.unique(pair_scores['var1'].to_list() + pair_scores['var2'].to_list()))
     return all_nodes, edges, g_list
+
+
+def _hot_deck_weights(distances):
+    """
+    Callable function that selects a random neighbor for every missing value. This function accepts an array of
+    distances, and returns an array of the same shape containing 0-1 weights.
+
+    Parameters
+    ----------
+    distances : `numpy.ndarray`
+        Array of distances of a recipient with its KNN neighbors.
+
+    Returns
+    ----------
+    weights : `numpy.ndarray`
+        Array with 0-1 weights.
+    """
+    weights = np.zeros(np.shape(distances))
+
+    # Randomly select the neighbor that will be used to impute each of the missing values
+    ids = np.random.randint(0, np.shape(distances)[1], size=np.shape(distances)[0])
+    if np.shape(distances)[0] == 1:
+        ids = [ids]
+
+    i = 0
+    for idx in ids:
+        weights[i][idx] = 1
+        i += 1
+
+    return weights
+
+
+def hot_deck_imputation(df, k=8, partitions=None):
+    """
+    Computes KNN hot deck imputation using sklearn KNNImputer
+    (see https://scikit-learn.org/stable/modules/generated/sklearn.impute.KNNImputer.html).
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame containing the data.
+    k : int, default=8
+        Number of neighbors.
+    partitions : `numpy.ndarray`, list of lists
+        Variable partitions/clusters to impute variables by cluster.
+        **Note** if partitions are passed, imputation is only performed on those observations with fewer than 1/4 of
+        their values within the partition variables missed.
+
+    Returns
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame with imputed values.
+    """
+    apply_threshold = True
+    if partitions is None:
+        apply_threshold = False
+        partitions = [set(list(df.columns))]
+
+    df = df.sample(frac=1, random_state=42)
+
+    mms = MinMaxScaler()
+    df = pd.DataFrame(mms.fit_transform(df), columns=df.columns)
+
+    for p in partitions:
+        p = list(p)
+        print(p)
+        imputer = KNNImputer(n_neighbors=8, weights=_hot_deck_weights)
+        df_p = df
+        if apply_threshold:
+            df_p = df[df[p].isnull().sum(1) <= np.floor(len(p) / 4)]
+        df_p_i = pd.DataFrame(data=imputer.fit_transform(df_p[p]), index=df_p.index, columns=p)
+        df.loc[df_p_i.index, df_p_i.columns] = df_p_i.values
+        print('Remaining missing values', df.isnull().sum().sum())
+
+    return df
---
+++ ldbx/data_processing/viz_utils.py
@@ -0,0 +1,133 @@
+# Utils for visualization
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import seaborn as sns
+
+from ..utils import savefig
+
+
+def plot_imputation_pairs_scatter(df, imputation_pairs, sample_frac=1.0, scatter_kws=None, line_kws=None,
+                                  output_path=None, savefig_kws=None):
+    """
+    Plots a grid of scatter plots with every pair of independent-dependent variables used for one-to-one model-based
+    imputation.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        DataFrame containing the data.
+    imputation_pairs : `pandas.DataFrame`
+        Imputation pairs as returned by the function `ldbx.data_processing.imputation_pairs()`.
+    sample_frac : float, default=1.0
+        If < 1 a random sample of every pair of variables will be plotted.
+    {scatter,line}_kws : dict, default=None
+        Additional keyword arguments to pass to the scatter and line plots.
+    output_path : str, default=None
+        Path to save figure as image.
+    savefig_kws : dict, default=None
+        Save figure options.
+    """
+    scatter_kws = scatter_kws if scatter_kws else dict(color='blue', alpha=0.05)
+    line_kws = line_kws if line_kws else dict(color='red')
+
+    ncols = min(4, imputation_pairs.shape[0])
+    nrows = int(np.ceil(imputation_pairs.shape[0] / ncols))
+
+    fig, axs = plt.subplots(nrows, ncols, figsize=(4 * ncols, 4 * nrows))
+    df_sample = df.sample(frac=sample_frac, random_state=42)
+    i = 0
+    for idx, row in imputation_pairs.iterrows():
+        ax = axs[i // ncols, i % ncols]
+        sns.regplot(x=row['var2'], y=row['var1'], data=df_sample, scatter_kws=scatter_kws, line_kws=line_kws, ax=ax)
+        ax.set_xlabel(f"{row['var2']} ({'{:.2f}'.format(row['missing_var2'])}% NA)")
+        ax.set_ylabel(f"{row['var1']} ({'{:.2f}'.format(row['missing_var1'])}% NA)")
+        i += 1
+
+    fig.tight_layout(pad=2)
+    savefig(output_path=output_path, savefig_kws=savefig_kws)
+
+
+def plot_imputation_distribution_assessment(df_prior, df_posterior, imputed_vars, sample_frac=1.0, prior_kws=None,
+                                            posterior_kws=None, output_path=None, savefig_kws=None):
+    """
+    Plots a distribution comparison of each variable with imputed variables, before and after imputation.
+
+    Parameters
+    ----------
+    df_prior : `pandas.DataFrame`
+        DataFrame containing the data before imputation.
+    df_posterior : `pandas.DataFrame`
+        DataFrame containing the data after imputation.
+    imputed_vars : list
+        List of variables with imputed variables.
+    sample_frac : float, default=1.0
+        If < 1 a random sample of every pair of variables will be plotted.
+    {prior,posterior}_kws : dict, default=None
+        Additional keyword arguments to pass to the kdeplot (https://seaborn.pydata.org/generated/seaborn.kdeplot.html).
+    output_path : str, default=None
+        Path to save figure as image.
+    savefig_kws : dict, default=None
+        Save figure options.
+    """
+    prior_kws = prior_kws if prior_kws else dict(color='#7F3C8D')
+    posterior_kws = posterior_kws if posterior_kws else dict(color='#11A579')
+
+    ncols = min(4, len(imputed_vars))
+    nrows = int(np.ceil(len(imputed_vars) / ncols))
+
+    fig, axs = plt.subplots(nrows, ncols, figsize=(4 * ncols, 4 * nrows))
+    df_prior_sample = df_prior.sample(frac=sample_frac, random_state=42)
+    i = 0
+    for ivar in imputed_vars:
+        ax = axs[i // ncols, i % ncols]
+        sns.kdeplot(ivar, data=df_prior_sample, label='Before imputation', ax=ax, **prior_kws)
+        sns.kdeplot(ivar, data=df_posterior.loc[df_prior_sample.index], label='After imputation', ax=ax,
+                    **posterior_kws)
+        ax.legend()
+        i += 1
+
+    fig.tight_layout(pad=2)
+    savefig(output_path=output_path, savefig_kws=savefig_kws)
+
+
+def plot_variable_graph_partitioning_components(edges, connected_components, graph_style_kws=None, output_path=None,
+                                                savefig_kws=None):
+    """
+    Plots a connected components of a graph.
+    **Note** this function relies on `ldbx.data_processing.variable_graph_partitioning()` for computing the edges and
+    connected components.
+
+    Parameters
+    ----------
+    edges : list of tuples
+        List of graph edges
+    connected_components : list of sets
+        List of connected components.
+    graph_style_kws : dict, default=None
+        Additional keyword arguments to style graph plots
+        (see https://networkx.org/documentation/stable/reference/drawing.html)
+    output_path : str, default=None
+        Path to save figure as image.
+    savefig_kws : dict, default=None
+        Save figure options.
+    """
+    if graph_style_kws is None:
+        graph_style_kws = dict(node_size=16, width=0.4, edge_color='grey', node_color='red', with_labels=True)
+
+    nrows = np.shape(connected_components)[0]
+    fig, axs = plt.subplots(nrows, 1, figsize=(12, 5*nrows))
+
+    for i in range(nrows):
+        fedges = []
+        for edge in edges:
+            if edge[0] in connected_components[i]:
+                fedges.append(edge)
+
+        G = nx.Graph()
+        G.add_nodes_from(connected_components[i])
+        G.add_edges_from(fedges)
+
+        nx.draw(G, ax=axs[i], **graph_style_kws)
+        savefig(output_path=output_path, savefig_kws=savefig_kws)
