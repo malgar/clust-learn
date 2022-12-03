46c9db51d27f16df7d0669cf7a647155fa9e7bc2
malgar <mglalvarezg@gmail.com>
2022-12-03 21:53:39 +0100

    setup updates

--- setup.py
+++ setup.py
@@ -1,5 +1,42 @@
 from setuptools import setup, find_packages
+import os
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(ROOT, 'README.md')) as f:
+    README = f.read()
+
+
 setup(
-    name='ldbx',
+    name='clust-learn',
+    version=1.0,
+    description="A Python package for explainable cluster analysis",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Miguel Alvarez-Garcia, Raquel Ibar-Alonso, Mar Arenas-Parra",
+    author_email="@gmail.com",
+    url="https://github.com/malgar/clust-learn",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GPLv3",
+        'Topic :: Machine Learning',
+        'Programming Language :: Python :: 3.9'
+    ],
+    license='GPLv3',
+    install_requires=[
+        "kneed==0.7.0",
+        "matplotlib==3.4.3",
+        "networkx==2.6.3",
+        "numpy==1.20.3",
+        "pandas==1.3.4",
+        "pingouin==0.5.1",
+        "prince==0.7.0",
+        "scikit-learn==1.0.2",
+        "scipy==1.7.1",
+        "seaborn==0.11.2",
+        "shap==0.40.0",
+        "statsmodels==0.13.2",
+        "xgboost==1.5.2"
+    ]
 )
