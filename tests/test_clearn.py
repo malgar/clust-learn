2d3e9869980d755a4d0c8a5e4ca7c0e336f76325
malgar <mglalvarezg@gmail.com>
2022-12-08 10:10:15 +0100

    tests

---
+++ tests/test_clearn.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class TestPreprocessing(TestCase):
+    def test_imputation(self):
+        self.assertEqual(0, 0)
+
+    def test_outliers(self):
+        self.assertEqual(0, 0)
+
+
+class TestDimReduction(TestCase):
+    def test_dim_reduction(self):
+        self.assertEqual(0, 0)
+
+
+class TestClustering(TestCase):
+    def test_clustering(self):
+        self.assertEqual(0, 0)
+
+
+class TestClassifier(TestCase):
+    def test_classifier(self):
+        self.assertEqual(0, 0)
