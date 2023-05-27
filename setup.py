fde66346a84b0064ce80038952d47960e98df013
malgar <mglalvarezg@gmail.com>
2023-05-27 08:21:24 +0200

    loosen the range of package versions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(ROOT, 'README.md')) as f:
 
 setup(
     name='clust-learn',
-    version=1.0,
+    version="0.1.3",
     description="A Python package for explainable cluster analysis",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -21,22 +21,25 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         'Topic :: Scientific/Engineering',
-        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
     ],
     license='GPLv3',
     install_requires=[
-        "kneed==0.7.0",
-        "matplotlib==3.4.3",
-        "networkx==2.6.3",
-        "numpy==1.20.3",
-        "pandas==1.3.4",
-        "pingouin==0.5.1",
+        "kneed>=0.7.0",
+        "matplotlib>=3.4.3",
+        "networkx>=2.6.3",
+        "numpy>=1.20.3",
+        "pandas>=1.3.4",
+        "pingouin>=0.5.1",
         "prince==0.7.0",
-        "scikit-learn==1.0.2",
-        "scipy==1.7.1",
-        "seaborn==0.11.2",
-        "shap==0.40.0",
-        "statsmodels==0.13.2",
-        "xgboost==1.5.2"
-    ]
+        "scikit-learn>=1.0.2",
+        "scipy>=1.7.1",
+        "seaborn>=0.11.2",
+        "shap>=0.40.0",
+        "statsmodels>=0.13.2",
+        "xgboost>=1.5.2"
+    ],
+    python_requires='>=3.9'
 )
