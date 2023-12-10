d4aa4bc489000242ff4e2e6fb6b2668755e4bc82
malgar <mglalvarezg@gmail.com>
2023-12-10 17:07:04 +0100

    Prepare new version release

--- README.md
+++ README.md
@@ -88,7 +88,7 @@ pip install clust-learn
 5. Version and license information
 </h2>
 
-* Version: 0.2.1
+* Version: 0.2.2
 * Author: Miguel Alvarez-Garcia (malvarez.statistics@gmail.com)
 * License: GPLv3 
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(ROOT, 'README.md')) as f:
 
 setup(
     name='clust-learn',
-    version="0.2.1",
+    version="0.2.2",
     description="A Python package for explainable cluster analysis",
     long_description=README,
     long_description_content_type="text/markdown",
