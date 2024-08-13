2e323d8e6bc883f604567c00af8e2c5c5cbdc42c
malgar <mglalvarezg@gmail.com>
2024-08-13 08:21:09 +0200

    Prepare new version release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(ROOT, 'README.md')) as f:
 
 setup(
     name='clust-learn',
-    version="0.2.5",
+    version="0.2.6",
     description="A Python package for explainable cluster analysis",
     long_description=README,
     long_description_content_type="text/markdown",
